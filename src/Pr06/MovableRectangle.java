7dbd64bad26d0ebd9bd13e9c4b5b87998cfb4be4
MishaShvetsov <70942871+MishaShvetsov@users.noreply.github.com>
2020-09-26 11:22:33 +0300

    Update MovableRectangle.java

--- src/Pr06/MovableRectangle.java
+++ src/Pr06/MovableRectangle.java
@@ -3,22 +3,43 @@ package Pr06;
 import Pr05.Rectangle;
 
 class MovableRectangle extends Rectangle implements Movable {
+
     private Point center;
+
     public MovableRectangle(boolean isFilled, String color, double width, double height) {
         super(isFilled, color, width, height);
         center = new Point(5, 3);
     }
 
+    @Override
+    public void String(int right, int down) {
+
+    }
+
     public Point getCenter() {
+
         return center;
     }
 
     public void setCenter(Point center) {
+
         this.center = center;
     }
 
     @Override
     public void move(int right, int down) {
         center.move(right, down);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "height=" + 1 +
+                ", width=" + 2 +
+                ", getHeight=" + getHeight() +
+                ", getWidth=" + getWidth() +
+                ", color=" + getColor() +
+                '}';
     }
 }
