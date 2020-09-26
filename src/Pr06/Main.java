3ec84e255e154d4247c2065226a4457ccd4ed5a2
MishaShvetsov <70942871+MishaShvetsov@users.noreply.github.com>
2020-09-26 11:10:29 +0300

    Update Main.java

--- src/Pr06/Main.java
+++ src/Pr06/Main.java
@@ -3,7 +3,9 @@ package Pr06;
 public class Main {
     public static void main(String[] args) {
         Movable p = new MovableRectangle(false, "red", 7, 2);
+        System.out.println(p);
         p.move(4, -2);
+        System.out.println(p);
 
     }
 }
